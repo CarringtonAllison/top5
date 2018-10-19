18c74f69fb149769d77f52514ec0bad086db8356
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2018-10-19 12:52:41 -0700

    carrington update

--- javascript/app.js
+++ javascript/app.js
@@ -6,54 +6,101 @@ var config = {
     projectId: "sleepers-9464b",
     storageBucket: "sleepers-9464b.appspot.com",
     messagingSenderId: "83781089592"
-  };
-  firebase.initializeApp(config);
-  
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
-        var database = firebase.database();
 
-      $('#btnSubmit').click(function() {
-        var u = $('#nameIpt').val();
-        var m = $('#msgIpt').val();
-        database.ref().push({name: u, text: m});
-        $('#msgIpt').val('');
-      });
+$(document).ready(function () {
+    $(".main-screen").hide();
+    $("#start").click(function () {
+        $(".main-screen").fadeIn();
+        $(this).hide();
+    });
+});
 
-      database.ref().on('child_added', function(snapshot) {
-        var msg = snapshot.val();
-        displayMsg(msg.name, msg.text);
-      });
 
-      function displayMsg(name, text) {
-        $('<div />').text(text).prepend($('<em/>').text(name+': ')).appendTo('#msgList');
-
-        $('#msgList')[0].scrollTop = $('#msgList')[0].scrollHeight;
-      };
-
-         
-      
-
+//Fantasy Football Nerd API 
 var apiKey = "ryz7dd7qeq6e";
 
 jQuery.ajaxPrefilter(function (options) {
-  if (options.crossDomain && jQuery.support.cors) {
-    options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-  }
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
 });
 
-var queryURL = "https://www.fantasyfootballnerd.com/service/players/json/" + apiKey + "/QB/1";
 
-// var queryURL = "https://fantasysports.yahooapis.com/fantasy/v2/league?client_id=" + apiKey;
-// console.log(queryURL);
-
-
+var queryURL = "https://www.fantasyfootballnerd.com/service/players/json/" + apiKey + "/QB";
 
 // https://cors-anywhere.herokuapp.com/
 $.ajax({
-  url: queryURL,
-  method: "GET"
+    url: queryURL,
+    method: "GET"
 }).then(function (response) {
-  console.log(response);
-  console.log("click");
+    // console.log(response);
+    // console.log("click");
 });
+
+
+//Position buttons
+$(".positionButtons").on("click", function () {
+
+    var posistion = $(this).attr("id")
+    console.log(posistion)
+
+
+    //Fantasy Football Nerd API 
+    var apiKey = "ryz7dd7qeq6e";
+
+    jQuery.ajaxPrefilter(function (options) {
+        if (options.crossDomain && jQuery.support.cors) {
+            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+        }
+    });
+
+    var queryURL = "https://www.fantasyfootballnerd.com/service/weekly-rankings/json/" + apiKey + "/" + posistion;
+
+    // https://cors-anywhere.herokuapp.com/
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log("click");
+    });
+
+
+
+
+
+
+})
+
+
+
+
+//chat submit button
+$('#btnSubmit').click(function () {
+    var u = $('#nameIpt').val();
+    var m = $('#msgIpt').val();
+    database.ref().push({ name: u, text: m });
+    $('#msgIpt').val('');
+});
+
+//when a msg is added it runs the displayMsg function
+database.ref().on('child_added', function (snapshot) {
+    var msg = snapshot.val();
+    displayMsg(msg.name, msg.text);
+});
+
+//dipslays the msg on the chat screen 
+function displayMsg(name, text) {
+    $('<div />').text(text).prepend($('<em/>').text(name + ': ')).appendTo('#msgList');
+
+    $('#msgList')[0].scrollTop = $('#msgList')[0].scrollHeight;
+};
+
+
+
+
 
