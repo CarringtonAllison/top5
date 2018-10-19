9f198bd474fc7ebf07426b0c9070338365008531
shaggsworth <austin.shea89@gmail.com>
2018-10-19 10:31:21 -0700

    austin_upload_2

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,24 @@
-var clientID = "dj0yJmk9Sjk5TUJPdW1Ganp2JmQ9WVdrOVptTkRVVEpwTm1jbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1kOQ--";
+var apiKey = "ryz7dd7qeq6e";
 
+jQuery.ajaxPrefilter(function (options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+  }
+});
 
-var queryURL = "https://fantasysports.yahooapis.com/fantasy/v2/league?client_id=" + clientID ;
-console.log(queryURL);
+var queryURL = "https://www.fantasyfootballnerd.com/service/players/json/" + apiKey + "/QB/1";
+
+// var queryURL = "https://fantasysports.yahooapis.com/fantasy/v2/league?client_id=" + apiKey;
+// console.log(queryURL);
 
 
 
 // https://cors-anywhere.herokuapp.com/
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      console.log("click");
-    });
-
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function (response) {
+  console.log(response);
+  console.log("click");
+});
 
