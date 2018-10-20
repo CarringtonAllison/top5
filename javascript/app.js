5a3141bd2c307e782e069f3f6da6bd0238eb5d98
Carrington Rain Allison <Carrington_Allison@yahoo.com>
2018-10-20 10:53:00 -0700

    Weekend Update

--- javascript/app.js
+++ javascript/app.js
@@ -1,135 +1,125 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDLh5dJYfHFziQJ5jTlTOdfLhKPjjIMkFA",
-  authDomain: "sleepers-9464b.firebaseapp.com",
-  databaseURL: "https://sleepers-9464b.firebaseio.com",
-  projectId: "sleepers-9464b",
-  storageBucket: "sleepers-9464b.appspot.com",
-  messagingSenderId: "83781089592"
+    apiKey: "AIzaSyDLh5dJYfHFziQJ5jTlTOdfLhKPjjIMkFA",
+    authDomain: "sleepers-9464b.firebaseapp.com",
+    databaseURL: "https://sleepers-9464b.firebaseio.com",
+    projectId: "sleepers-9464b",
+    storageBucket: "sleepers-9464b.appspot.com",
+    messagingSenderId: "83781089592"
 };
 firebase.initializeApp(config);
 var database = firebase.database();
+var apiKey = "ryz7dd7qeq6e";
+
+//cors hack
+jQuery.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+
 
 //start button hiding everything 
 $(document).ready(function () {
-  $(".main-screen").hide();
-  $("#start").click(function () {
-    $(".main-screen").fadeIn();
-    $(this).hide();
-  });
+    $(".main-screen").hide();
+    $("#start").click(function () {
+        $(".main-screen").fadeIn();
+        $(this).hide();
+    });
 });
 
 
-//Fantasy Football Nerd API 
 
-jQuery.ajaxPrefilter(function (options) {
-  if (options.crossDomain && jQuery.support.cors) {
-    options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-  }
-});
-
-var apiKey = "ryz7dd7qeq6e";
-
+//when the start button is clicked
 $("#start").on("click", function () {
 
-  var queryURL = "https://www.fantasyfootballnerd.com/service/weekly-rankings/json/" + apiKey + "/QB";
+    var queryURL = "https://www.fantasyfootballnerd.com/service/weekly-rankings/json/" + apiKey + "/QB";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response);
-    for (var i = 0; i < response.Rankings.length; i++) {
-      var tRow = $("<tr>");
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        rank = response.Rankings;
 
-      var playerPosition = $("<td>").text(response.Rankings[i].position);
-
-      var playerName = $("<td>").text(response.Rankings[i].name);
-
-      var playerTeam = $("<td>").text(response.Rankings[i].team);
-
-      var playerStandard = $("<td>").text(response.Rankings[i].standard);
-
-      tRow.append(playerPosition, playerName, playerTeam, playerStandard);
-
-      $("tbody").append(tRow);
-    }
-
-
-
-
-  });
-
+        for (var i = 0; i < 5; i++) {
+            var tRow = $("<tr>");
+            var playerPosition = $("<td>").text(rank[i].position);
+            var playerName = $("<td>").text(rank[i].name);
+            var playerTeam = $("<td>").text(rank[i].team);
+            var playersScore = $("<td>").text(rank[i].standard);
+            playersScore.addClass("scores")
+            playersScore.attr("data-ppr", rank[i].ppr);
+            playersScore.attr("data-standard", rank[i].standard);
+            tRow.append(playerPosition, playerName, playerTeam, playersScore);
+            $("tbody").append(tRow);
+        }
+    });
+    
 });
 
 
 //Position buttons
 $(".positionButtons").on("click", function () {
-
-  var posistion = $(this).attr("id")
-  console.log(posistion)
-
-});
-
-//Fantasy Football Nerd API 
-
-
-
-// var queryURL = "https://www.fantasyfootballnerd.com/service/weekly-rankings/json/" + apiKey;
-
-// // https://cors-anywhere.herokuapp.com/
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function (response) {
-
-    //Fantasy Football Nerd API 
-    var apiKey = "ryz7dd7qeq6e";
-
-
-// });
-
-    // https://cors-anywhere.herokuapp.com/
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     // console.log(response);
-    //     // console.log("click");
-    //     var rank = response.Rankings 
-    //     // console.log(rank)
-    //     for (var i = 0; i < 5; i++){
-    //         console.log(rank[i])
-    //     }
-
-    // });
+    $("#player").empty();
+    var position = $(this).attr("id")
+    var queryURL = "https://www.fantasyfootballnerd.com/service/weekly-rankings/json/" + apiKey + "/" + position;
     
-
-
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        rank = response.Rankings;
+        
+        for (var i = 0; i < 5; i++) {
+            var tRow = $("<tr>");
+            var playerPosition = $("<td>").text(rank[i].position);
+            var playerName = $("<td>").text(rank[i].name);
+            var playerTeam = $("<td>").text(rank[i].team);
+            var playersScore = $("<td>").text(rank[i].standard);
+            playersScore.addClass("scores");
+            playersScore.attr("data-ppr", rank[i].ppr);
+            playersScore.attr("data-standard", rank[i].standard);
+            tRow.append(playerPosition, playerName, playerTeam, playersScore);
+            $("tbody").append(tRow).fadeIn();
+        }
+    });
+});
 
 
 //chat submit button
 $('#btnSubmit').click(function () {
-  var u = $('#nameIpt').val();
-  var m = $('#msgIpt').val();
-  database.ref().push({ name: u, text: m });
-  $('#msgIpt').val('');
+    var u = $('#nameIpt').val();
+    var m = $('#msgIpt').val();
+    database.ref().push({ name: u, text: m });
+    $('#msgIpt').val('');
 });
 
 //when a msg is added it runs the displayMsg function
 database.ref().on('child_added', function (snapshot) {
-  var msg = snapshot.val();
-  displayMsg(msg.name, msg.text);
+    var msg = snapshot.val();
+    displayMsg(msg.name, msg.text);
 });
 
 //dipslays the msg on the chat screen 
 function displayMsg(name, text) {
-  $('<div />').text(text).prepend($('<em/>').text(name + ': ')).appendTo('#msgList');
-
-  $('#msgList')[0].scrollTop = $('#msgList')[0].scrollHeight;
+    $('<div />').text(text).prepend($('<em/>').text(name + ': ')).appendTo('#msgList');
+    
+    $('#msgList')[0].scrollTop = $('#msgList')[0].scrollHeight;
 };
 
 
+//The League buttons 
+$("#ppr").on("click", function(){
+    console.log("working");
+    $(".scores").empty();
+    $(".scores").text($(".scores").attr("data-ppr"))
+})
 
+$("#standard").on("click", function(){
+    console.log("working");
+    $(".scores").empty();
+    $(".scores").text($(".scores").attr("data-standard"))
+})
 
 
