3cac401ec9638da2dc07c2fe5d2e0d10af16289a
shaggsworth <austin.shea89@gmail.com>
2018-10-21 21:46:35 -0700

    austin_10_21_upload

--- javascript/app.js
+++ javascript/app.js
@@ -20,6 +20,7 @@ jQuery.ajaxPrefilter(function (options) {
     }
 });
 
+
 //start button hiding everything 
 $(document).ready(function () {
     $(".main-screen").hide();
@@ -32,11 +33,10 @@ $(document).ready(function () {
 });
 
 
-
-// when the start button is clicked
+//when the start button is clicked
 $("#start").on("click", function () {
-    favTeamColor();
-    
+
+    //Fantasy Football Nerd API
     var queryURL = "https://www.fantasyfootballnerd.com/service/weekly-rankings/json/" + fantasyApiKey + "/QB";
 
     $.ajax({
@@ -58,10 +58,49 @@ $("#start").on("click", function () {
             $("tbody").append(tRow);
         }
     });
-    
+
+    var zip = $("#zipCode").val();
+
+
+    // Yelp API 
+    var yelpUrl = "https://api.yelp.com/v3/businesses/search?location=" + zip + "&categories=sportsbars&limit=5"
+
+    $.ajax({
+        url: yelpUrl,
+        headers: {
+            'Authorization': 'Bearer DEarLC7CAG_qsAEt1-nz8iyDsLL2tcDUn72S1wje4GmAksA05LKkj2MpqqyyNzFMdiysoE-Nv_wTUGQQn-rYM5scXqQpdFu-mObJuPPtwyiGXrqLWPGAYdrG-q7LW3Yx',
+        },
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            // console.log(data);
+            sportsBar = data.businesses
+            console.log(sportsBar)
+
+            for (var i = 0; i < 5; i++) {
+                var $div = $("<div>");
+                $div.addClass("col d-flex justify-content-center layout");
+                var $2div = $("<div>");
+                $2div.addClass("card bars");
+                $2div.attr("data-url", sportsBar[i].url);
+                var $img = $("<img>");
+                $img.addClass("card-img-top img-size");
+                $img.attr("src", sportsBar[i].image_url);
+                $img.attr("alt", sportsBar[i].name);
+                var $3div = $("<div>");
+                $3div.addClass("card-body");
+                var $p = $("<p>");
+                $p.addClass("card-text");
+                $p.attr("id", "barInfo")
+                $p.append("<h5>" + sportsBar[i].name + "</h5>", "<p class='barText'>" + sportsBar[i].location.display_address + "</p>", "<p class='barText'>" + "Phone: " + sportsBar[i].phone + "</p>");
+                $div.append($2div);
+                $2div.append($img, $3div);
+                $3div.append($p);
+                $("#barListing").append($div);
+            }
+        }
+    });
 });
-
-
 
 
 //Position buttons
@@ -69,14 +108,14 @@ $(".positionButtons").on("click", function () {
     $("#player").empty();
     var position = $(this).attr("id")
     var queryURL = "https://www.fantasyfootballnerd.com/service/weekly-rankings/json/" + fantasyApiKey + "/" + position;
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response)
         rank = response.Rankings;
-        
+
         for (var i = 0; i < 5; i++) {
             var tRow = $("<tr>");
             var playerPosition = $("<td>").text(rank[i].position);
@@ -110,7 +149,6 @@ database.ref().on('child_added', function (snapshot) {
 //dipslays the msg on the chat screen 
 function displayMsg(name, text) {
     $('<div />').text(text).prepend($('<em/>').text(name + ': ')).appendTo('#msgList');
-    
     $('#msgList')[0].scrollTop = $('#msgList')[0].scrollHeight;
 };
 
@@ -128,62 +166,23 @@ $("#standard").on("click", function () {
     $(".scores").text($(".scores").attr("data-standard"))
 })
 
+// if the yelp results are clicked then they will open another page with the yelp website 
+$(document).on("click", ".bars", function () {
+    var barUrl = $(this).attr("data-url");
+    console.log(barUrl);
+    window.open(barUrl)
 
-// this is the YELP api testing on a button
-$("#yelp").on("click", function () {
-    console.log("working")
-    //This url is set for 85233 zip right now but we can set a variable for that to change with input 
-    var yelpUrl = "https://api.yelp.com/v3/businesses/search?location=85233&categories=sportsbars&limit=5"
-    
-    $.ajax({
-        url: yelpUrl,
-        headers: {
-            'Authorization': 'Bearer DEarLC7CAG_qsAEt1-nz8iyDsLL2tcDUn72S1wje4GmAksA05LKkj2MpqqyyNzFMdiysoE-Nv_wTUGQQn-rYM5scXqQpdFu-mObJuPPtwyiGXrqLWPGAYdrG-q7LW3Yx',
-        },
-        method: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            // console.log(data);
-            sportsBar = data.businesses
-            console.log(sportsBar)
-
-            for (var i = 0; i < 5; i++) {
-                var $div = $("<div>");
-                $div.addClass("col d-flex justify-content-center layout");
-                var $2div = $("<div>");
-                $2div.addClass("card bars");
-                var $img = $("<img>");
-                $img.addClass("card-img-top img-size");
-                $img.attr("src", sportsBar[i].image_url);
-                $img.attr("alt", sportsBar[i].name);
-                var $3div = $("<div>");
-                $3div.addClass("card-body");
-                var $p = $("<p>");
-                $p.addClass("card-text");
-                $p.attr("id", "barInfo")
-                $p.append("<h4>" + sportsBar[i].name + "</h4>", "<p class='barText'>" + sportsBar[i].location.display_address + "</p>", "<p class='barText'>" + "Phone: " + sportsBar[i].phone + "</p>");
-                $div.append($2div);
-                $2div.append($img, $3div);
-                $3div.append($p);
-                $("#barListing").append($div);
-            }
-
-
-        }
-    });
-    
-});
+})
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////// PUT NEW CODE BELOW ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 var teamLogos = {
 
     cardinals: "https://mk0teamcolorcodtgc6i.kinstacdn.com/wp-content/uploads/2017/05/Arizona_Cardinals_logo.jpg",
-    
+
     falcons: "https://mk0teamcolorcodtgc6i.kinstacdn.com/wp-content/uploads/2017/05/Atlanta_Falcons_logo.jpg",
 
     ravens: "https://mk0teamcolorcodtgc6i.kinstacdn.com/wp-content/uploads/2017/05/Baltimore_Ravens_logo.jpg",
@@ -248,34 +247,188 @@ var teamLogos = {
 
     redskins: "https://mk0teamcolorcodtgc6i.kinstacdn.com/wp-content/uploads/2017/05/Washington_Redskins_logo.jpg",
 
-    
-}
 
+}
+favTeamColor()
 
 function favTeamColor() {
     console.log("click");
     // console.log(this);
+    // var logos = $("<image>").attr("class", "logos");
+    // $(".logos").attr("src", teamLogos.cardinals)
+    // $(".main-screen").append(logos);
 
-    $(".dropdown-item").on("click", function() {
-        console.log(this);
+    $(".dropdown-item").on("click", function () {
         var teamSelected = $(this).attr("value");
         console.log(teamSelected);
         if (teamSelected === "Arizona Cardinals") {
             $("body").stop().animate({
-                backgroundColor: "rgba(151,35,63, 0.6)",
+                backgroundColor: "rgb(151,35,63)",
                 color: "rgb(255,182,18)"
-            }, 1000)
-            console.log("click");
-        }
-        
-   
-    })
-  
-    // if ($(".team-list:selected").val() === "Arizona Cardinals") {
-    //     console.log("click");
-    //     $("body").animate({backgroundColor: "red"}, "slow")
-    // }
-    
+            }, 1000);
+        } else if (teamSelected === "Atlanta Falcons") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(167,25,48)",
+                color: "rgb(0, 0, 0)"
+            }, 1000);
+        } else if (teamSelected === "Baltimore Ravens") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(26,25,95)",
+                color: "rgb(158,124,12)"
+            }, 1000);
+        } else if (teamSelected === "Buffalo Bills") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,51,141)",
+                color: "rgb(198,12,48)"
+            }, 1000);
+        } else if (teamSelected === "Carolina Panthers") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,133,202)",
+                color: "rgb(16,24,32)"
+            }, 1000);
+        } else if (teamSelected === "Chicago Bears") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(11,22,42)",
+                color: "rgb(0,75,100,0)"
+            }, 1000);
+        } else if (teamSelected === "Cincinnati Bengals") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(251,79,20,2)",
+                color: "rgb(0,0,0)"
+            }, 1000);
+        } else if (teamSelected === "Cleveland Browns") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(49,29,0)",
+                color: "rgb(255,60,0)"
+            }, 1000);
+        } else if (teamSelected === "Dallas Cowboys") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,53,148)",
+                color: "rgb(134,147,151)"
+            }, 1000);
+        } else if (teamSelected === "Dallas Cowboys") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,53,148)",
+                color: "rgb(134,147,151)"
+            }, 1000);
+        } else if (teamSelected === "Denver Broncos") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,34,68)",
+                color: "rgb(251,79,20)"
+            }, 1000);
+        } else if (teamSelected === "Detroit Lions") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,118,182)",
+                color: "rgb(176,183,188)"
+            }, 1000);
+        } else if (teamSelected === "Green Bay Packers") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(24 48 40)",
+                color: "rgb(255,184,28)"
+            }, 1000);
+        } else if (teamSelected === "Houston Texans") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(3,32,47)",
+                color: "rgb(167,25,48)"
+            }, 1000);
+        } else if (teamSelected === "Indianapolis Colts") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,44,95)",
+                color: "rgb(162,170,173)"
+            }, 1000);
+        } else if (teamSelected === "Jacksonville Jaguars") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,103,120)",
+                color: "rgb(159,121,44)"
+            }, 1000);
+        } else if (teamSelected === "Kansas City Chiefs") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(227,24,55)",
+                color: "rgb(255,184,28)"
+            }, 1000);
+        } else if (teamSelected === "Los Angeles Chargers") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,128,198)",
+                color: "rgb(255,194,14)"
+            }, 1000);
+        } else if (teamSelected === "Los Angeles Rams") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,34,68)",
+                color: "rgb(134,109,75)"
+            }, 1000);
+        } else if (teamSelected === "Miami Dolphins") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,142,151)",
+                color: "rgb(242,106,36)"
+            }, 1000);
+        } else if (teamSelected === "Minnesota Vikings") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(79,38,131)",
+                color: "rgb(255,198,47)"
+            }, 1000);
+        } else if (teamSelected === "New England Patriots") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,34,68)",
+                color: "rgb(198,12,48)"
+            }, 1000);
+        } else if (teamSelected === "New Orleans Saints") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(211,188,141)",
+                color: "rgb(16,24,31)"
+            }, 1000);
+        } else if (teamSelected === "New York Giants") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(1,35,82)",
+                color: "rgb(163,13,45)"
+            }, 1000);
+        } else if (teamSelected === "New York Jets") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,63,45)",
+                color: "rgb(39,37,31)"
+            }, 1000);
+        } else if (teamSelected === "Oakland Raiders") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,0,0)",
+                color: "rgb(165,172,175)"
+            }, 1000);
+        } else if (teamSelected === "Philadelphia Eagles") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,76,84)",
+                color: "rgb(95,96,98)"
+            }, 1000);
+        } else if (teamSelected === "Pittsburgh Steelers") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(255,182,18)",
+                color: "rgb(16,24,32)"
+            }, 1000);
+        } else if (teamSelected === "San Francisco 49ers") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(170,0,0)",
+                color: "rgb(173,153,93)"
+            }, 1000);
+        } else if (teamSelected === "Seattle Seahawks") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(105,190,40)",
+                color: "rgb(0,34,68)"
+            }, 1000);
+        } else if (teamSelected === "Tampa Bay Buccaneers") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(213,10,10)",
+                color: "rgb(52,48,43)"
+            }, 1000);
+        } else if (teamSelected === "Tennessee Titans") {
+            $("body").stop().animate({
+                backgroundColor: "rgb(0,42,92)",
+                color: "rgb(68,149,209)"
+            }, 1000);
+        } else  {
+            $("body").stop().animate({
+                backgroundColor: "rgb(63,16,16)",
+                color: "rgb(255,182,18)"
+            }, 1000);
+        } 
+
+    });
+
+
 };
-
-
